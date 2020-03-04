67071aaf9ac5bb38565a923aca6bd5a01c39f4c9
Paul Aguilar <pauleaguilar@gmail.com>
2020-03-04 01:58:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -142,7 +142,7 @@ end
 
 
 def num_points_scored(player_name)
-  game_hash.values.each do |team|  
+  game_hash.values.each do |team|
     team[:players].each do |player|
       return player[:points] if player.has_value?(player_name)
     end
