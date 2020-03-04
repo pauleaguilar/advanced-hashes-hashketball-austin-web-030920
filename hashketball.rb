1e0487e2f7eed60f1c30b528e800798b2c561b7d
Paul Aguilar <pauleaguilar@gmail.com>
2020-03-04 02:31:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,15 +189,18 @@ end
 
 
 def big_shoe_rebounds
-  big_shoes_guy = 0
-  rebounds = 0
-    game_hash.each do | team, team_details_hash|
-      team_details_hash[:players].each do | stats |
-        if stats[:shoe] > big_shoes_guy
-          big_shoes_guy = stats[:shoe]
-          rebounds = stats[:rebounds]
+    player_shoe = {}
+    game_hash.each do |location, team_data|
+        team_data[:players].each do |player, player_data|
+            player_shoe[player] = player_data[:shoe]
         end
-      end
     end
-  rebounds
+    max_size = player_shoe.values.max
+    game_hash.each do |location, team_data|
+        team_data[:players].each do |player, player_data|
+            if player_data[:shoe] == max_size
+                return player_data[:rebounds]
+            end
+        end
+    end
 end
